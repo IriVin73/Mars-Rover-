a893625bec8efc08210dbae25b65f8d4c0c7c730
IriVin73 <irishavinod@gmail.com>
2025-03-04 22:03:20 +0000

    All inputs validated and saved.

---
+++ Coordinates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Rover_Assessment
+{
+    class Coordinates
+    {
+        private string Movement;
+        private int MaxX;
+        private int MaxY;
+        private int StartX;
+        private int StartY;
+        private char StartDirection;
+
+        public Coordinates() { }
+
+        public void SetMax(string UpperRight)
+        {
+            string[] tokens = UpperRight.Split(' ');
+            MaxX = int.Parse(tokens[0]);
+            MaxY = int.Parse(tokens[1]);
+        }
+
+        public void SetStart(string StartPoint)
+        {
+            string[] tokens = StartPoint.Split(' ');
+            StartX = int.Parse(tokens[0]);
+            StartY = int.Parse(tokens[1]);
+            StartDirection = char.Parse(tokens[2]);
+        }
+
+        public void SetMovement(string Movement)
+        { this.Movement = Movement; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,15 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Mars_Rover_Assessment
 {
     internal class Program
     {
+         public static Coordinates Coords = new Coordinates();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert");
+            AskUpperRight();
+        }
+
+        private static void AskUpperRight()
+        {
+            Console.WriteLine("Insert upper right coordinates of plateau:");
+            string UpperRight = Console.ReadLine();
+            CheckUpperRight(UpperRight);
+        }
+
+        private static void CheckUpperRight(string UpperRight)
+        {
+            //checks that input is in the form number,space,number
+            if (Regex.IsMatch(UpperRight, @"^\d+\s\d+$"))
+            {
+                Coords.SetMax(UpperRight);
+                AskStartPoint();
+            }
+            else
+            {
+                Console.WriteLine("Invalid format.");
+                AskUpperRight();
+            }
+        }
+
+        private static void AskStartPoint()
+        {
+            Console.WriteLine("Insert rover start point and heading:");
+            string StartPoint = Console.ReadLine();
+            CheckStartPoint(StartPoint);
+        }
+
+        private static void CheckStartPoint(string StartPoint)
+        {
+            //checks that input is in the form number,space,number,space,capital letter
+            if (Regex.IsMatch(StartPoint, @"^\d+\s\d+\s[NSEW]$"))
+            {
+                Coords.SetStart(StartPoint);
+                AskMovement();
+            }
+            else
+            {
+                Console.WriteLine("Invalid format.");
+                AskStartPoint();
+            }
+        }
+
+        private static void AskMovement()
+        {
+            Console.WriteLine("Insert movement commands:");
+            string Movement = Console.ReadLine();
+            CheckMovement(Movement);
+        }
+
+        private static void CheckMovement(string Movement)
+        {
+            if(Regex.IsMatch(Movement, @"^[LMR]+$"))
+            {
+                Coords.SetMovement(Movement);
+            }
+            else
+            {
+                Console.WriteLine("Invalid format.");
+                AskMovement();
+            }
         }
     }
 }
